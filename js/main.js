67ba332a0fce8d821112f5a2a2aaceb9e73fdcc5
Kevin Gu <kevgu@publicisgroupe.net>
2016-11-29 16:11:00 +0800

    add two animations on swipeWrap & js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-	$(".startWrap .ageWrap .yesBtn").bind('click',function(){
-		$(".startWrap .ageWrap").fadeOut();
-		$(".startWrap .swipeWrap").fadeIn();
-	});
+	//init
+	var JC = {};
+	JC.gender = "";
 
 	var quizSwiper = new Swiper('.swiper-container',
 		{
@@ -16,6 +15,109 @@ $(document).ready(function(){
     			swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
   			}
   	});
-
 	
+	/*
+		at startWrap click on yes button
+		setup those animations' timeline
+	*/
+	$(".startWrap .ageWrap .yesBtn").bind('click',function(){
+		$(".startWrap .ageWrap").fadeOut();
+		$(".startWrap .swipeWrap").fadeIn();
+		if($(".swipeWrap").is(":visible")){
+			setTimeout(function(){
+				$(".swipeWrap .vicent-pop").addClass("animated");
+			},1000);
+			setTimeout(function(){
+				$(".swipeWrap .vicent-pop").fadeOut().removeClass(".animated");
+				$(".swipeWrap .swipePrompt").addClass("animated");
+				var ele = document.getElementById('swipeWrap');
+				detectSwipe(ele,60,switchToQuestion);
+			},2500);
+
+			
+		}
+		
+	});
+
+
+	//callback function after detectSwipe and get the gender based on direction
+	function switchToQuestion(dir){
+		$(".quizWrap").addClass("show");
+		if(dir === "left"){
+			JC.gender = "male";
+			$(".quizWrap").find(".quiz-8").addClass('male');
+		}else if(dir === "right"){
+			JC.gender = "female";
+			$(".quizWrap").find(".quiz-8").addClass('female');
+		}
+		$(".startWrap").fadeOut();
+	}
+
+	//detect swipe direction on selected element
+	function detectSwipe(ele,dis,func){
+		if(!ele){ele = document;}
+		ele.addEventListener('touchstart', handleTouchStart, false); 
+		ele.addEventListener('touchmove', handleTouchMove, false);
+		ele.addEventListener('touchend', handleTouchEnd, false);
+
+		var xStart = null, 	
+			yStart = null,
+			xEnd = null,
+    		yEnd = null,
+	    	xDiff = 0,
+	    	yDiff = 0;
+
+		var dir = "";
+		var touchMoved = false;
+		var diff = dis || 60;
+
+		function handleTouchStart(e){                                         
+			xStart = e.touches[0].clientX;                                      
+			yStart = e.touches[0].clientY;                                      
+		};                                                
+		function handleTouchMove(e){
+			e.preventDefault();
+			xEnd = e.touches[0].clientX,                                  
+			yEnd = e.touches[0].clientY;
+			//the click event will be called with touchstart and touchend, we need to detect whether the touch has moved
+			touchMoved = true;
+		}
+
+		function handleTouchEnd(e){
+			if(touchMoved == false){
+				return;
+			}
+			xDiff = xStart - xEnd,
+			yDiff = yStart - yEnd;
+
+			if (Math.abs(xDiff) > Math.abs(yDiff) ) {/*most significant*/
+				if(xDiff > 0 && xDiff > diff) {
+   					dir = "left";
+				}else if(xDiff < 0 && Math.abs(xDiff) > diff){
+    				dir = "right";
+				}else{
+					return;
+				}                       
+			}else{
+				if(yDiff > 0 && yDiff > diff) {
+    				dir = "up";
+				}else if(yDiff < 0 && Math.abs(yDiff) > diff){ 
+  	  				dir = "down";
+        		}else{
+        			return;
+        		}
+			}
+
+			if(dir !== ""){
+				if(typeof func === 'function'){
+					func(dir);
+				}
+			}
+			xStart = null;
+			yStart = null;
+			xEnd = null;
+			yEnd = null;  
+			touchMoved = false;                                           
+		};
+	}
 });
