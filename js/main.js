502197fe60a17b96a6200d3f00f38e435102e4c2
Kevin Gu <kevgu@publicisgroupe.net>
2016-12-02 17:13:56 +0800

    1.re-cut images 2.fixed minor bugs in js 3.css for animation later;

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
 	var JC = {};
 	JC.swiper = null;
 	JC.gender = "";
-
-	JC.resultContent = [{'type':'calculative','title':'社交心机党','content':'冷眼旁观种种奇葩,无言以对各路槽点囧事，想靠装腔social红出一片天？,腻害了你！'},
+	JC.getAward = false;
+	JC.resultContent = [{'type':'calculative','title':'社交心机党','content':'冷眼旁观种种奇葩,无言以对各路槽点囧事,想靠装腔social红出一片天？,腻害了你！'},
 						{'type':'warmUp','title':'天生暖场王','content':'秒懂各路梗,深解内涵图,随口解救种种冷场危机！'},
 						{'type':'babyBicao', 'title':'壁草宝宝','content':'不想怒刷存在感,就在一边安安静静,做个萌宝吧！'},
 						{'type':'topicMaker','title':'话题制燥者','content':'上知票房毒药神演技,下知麻豆爱豆八卦史,叨逼叨是天性,分分钟撩人没鸭梨！'}];
 	var count = 0;
+	//get user's openid at the begining
+	getOpenId();
 	/*
 		at startWrap click on yes button
 		setup those animations' timeline
@@ -17,6 +19,8 @@ $(document).ready(function(){
 		if($(this).hasClass("yesBtn")){
 			$(".startWrap .ageWrap").fadeOut();
 			$(".startWrap .swipeWrap").fadeIn();
+			/*load background image for quizWrap*/
+			$(".quizWrap .bgWrap").addClass('loaded');
 			if($(".swipeWrap").is(":visible")){
 				setTimeout(function(){
 					$(".swipeWrap .vicent-pop").addClass("animated");
@@ -25,8 +29,10 @@ $(document).ready(function(){
 				setTimeout(function(){
 					$(".swipeWrap .vicent-pop").hide().removeClass(".animated");
 					$(".swipeWrap .swipePrompt").addClass("animated");
+					/*load background image for quizWrap*/
 					var ele = document.getElementById('swipeWrap');
 					detectSwipe(ele,60,switchToQuestion);
+
 				},4900);		
 			}	
 		}else{
@@ -37,11 +43,16 @@ $(document).ready(function(){
 
 	//click events for answering question
 	$(".quizWrap .swiper-slide .answerWrap").bind('click',function(){
-		var currentQuiz = $(this).parent('.swiper-slide').attr('data-quiz');
-		var currentAnswer = $(this).attr('data-an');
+		var currentQuiz = $(this).parent('.swiper-slide').attr('data-quiz'),
+			currentAnswer = $(this).attr('data-an'),
+			currentWrap = $(this);
+
+		
 		//for single answer quiz
 		if(currentQuiz !== '6' && currentQuiz !== '8'){
-			
+			if(currentQuiz === '7'){
+				gernerateResult(currentAnswer);
+			}
 			console.log("currentQuiz "+currentQuiz);
 			console.log("currentAnswer " + currentAnswer);
 			JC.swiper.slideNext();
@@ -49,9 +60,14 @@ $(document).ready(function(){
 		}else if(currentQuiz === '6'){
 			var firstAnswer =  currentAnswer;
 			if(count !== 2){
-				console.log("currentQuiz "+currentQuiz);
-				console.log("currentAnswer " + currentAnswer);
-				count ++;
+				if(currentWrap.hasClass('checked')){
+					alert('这个选项已被选择');
+					return;
+				}else{
+					console.log("currentQuiz "+currentQuiz);
+					console.log("currentAnswer " + currentAnswer);
+					count ++;
+				}
 			}
 			if(count === 2){
 				JC.swiper.slideNext();
@@ -60,14 +76,14 @@ $(document).ready(function(){
 				uploadAnswer(currentQuiz, answer);	
 			}
 		}else if(currentQuiz === '8'){
-			gernerateResult(currentAnswer);
 			console.log("currentQuiz "+currentQuiz);
 			console.log("currentAnswer " + currentAnswer);
 			uploadAnswer(currentQuiz, currentAnswer);	
 			$('.quizWrap').fadeOut();
-			$('.resultWrap').fadeIn();
+			$('.resultWrap').show();
 			showResultWrap();
 		}
+		currentWrap.addClass('checked');
 	});
 
 
@@ -83,11 +99,7 @@ $(document).ready(function(){
 			},4200);
 		},1000);
 
-		$('.resultWrap .result .giftBtn').bind('click',function(){
-			$('.resultWrap .result').hide();
-			$('.resultWrap .awardWrap').fadeIn();
-		});
-
+		//award clostBtn
 		$('.resultWrap .awardWrap .closeBtn').bind('click',function(){
 			$('.resultWrap .awardWrap').fadeOut();
 			if(!$('.resultWrap .result').is(':visible')){
@@ -95,15 +107,74 @@ $(document).ready(function(){
 			}
 		});
 
-		$('.resultWrap .awardWrap .info #phone').bind('keydown keyup',function(e){
+		//get giftBtn show info inputs
+		$('.resultWrap .result .giftBtn').bind('click',function(){
+			$('.resultWrap .result').hide();
+			$('.resultWrap .awardWrap').fadeIn();
+		});
+
+		//phone input
+		$('.resultWrap .awardWrap .info input.limited').keypress(function(e){
 			var maxlength = $(this).attr('data-maxlength');
 			var len = $(this).val().length;
-			if(len === maxlength){
+			if(len == maxlength){
 				e.preventDefault();
 			}else if(len > maxlength){
-				$(this).val($(this).val().substr(0,maxlength));
-				
+				var s = $(this).val().substring(0,maxlength);
+				$(this).val(s);
 			}
+		});
+
+		$('.resultWrap .awardWrap .info .getCodeBtn').bind('click',function(e){
+			var phoneNumber = $('.resultWrap .awardWrap .info #phone').val();
+			if(phoneNumber.length == 0){
+				alert('请先输入手机号');
+				return;
+			}else if(!isValidPhone(phoneNumber)){
+				alert('请输入正确的手机号');
+				return;
+			}else if(isValidPhone(phoneNumber)){
+				getVertifyCode(phoneNumber);
+			}
+		});
+
+		$('.resultWrap .awardWrap .info .submitBtn').bind('click',function(e){
+
+			var phoneNumber = $('.resultWrap .awardWrap .info #phone').val(),
+				code = $('.resultWrap .awardWrap .info #code').val(),
+				username =  $('.resultWrap .awardWrap .info #fullname').val();
+			if(!phoneNumber.length){
+				alert('请先输入手机号');
+				return;
+			}else if(!isValidPhone(phoneNumber)){
+				alert('请输入正确的手机号');
+				return;
+			}else if(code.length != 4){
+				alert('请输入4位验证码');
+				return;
+			}else if(isValidPhone(phoneNumber) && code.length == 4){
+				submitInfo(phoneNumber,code);
+			}
+		});
+
+		$('.resultWrap .awardWrap .award .submitBtn').bind('click',function(e){
+
+			var add = $('.resultWrap .awardWrap .award #add').val();
+			if(!add.length){
+				alert('请输入您的地址');
+			}else{
+				uploadAddress(add);
+			}	
+		});
+
+		$('.resultWrap .awardWrap .tryBtn').bind('click',function(e){
+			$('.resultWrap .awardWrap').hide();
+			$('.resultWrap .shareWrap').fadeIn();
+		});
+
+		$('.resultWrap .shareWrap').bind('click',function(e){
+			$('.resultWrap .shareWrap').hide();
+			$('.resultWrap .awardWrap').fadeIn();
 		});
 	}
 
@@ -117,7 +188,7 @@ $(document).ready(function(){
 			noSwipingClass : 'stop-swiping',	
 			onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
     			swiperAnimateCache(swiper); //隐藏动画元素 
-    				swiperAnimate(swiper); //初始化完成开始动画
+    			swiperAnimate(swiper); //初始化完成开始动画
 
     		}, 
   			onSlideChangeEnd: function(swiper){ 
@@ -132,6 +203,8 @@ $(document).ready(function(){
 			$(".quizWrap").find(".quiz-8").addClass('female');
 		}
 		$(".startWrap").fadeOut();
+		/*load background image for resultWrap*/
+		$(".resultWrap").addClass('loaded');
 	}
 
 
@@ -222,7 +295,7 @@ $(document).ready(function(){
 
 	function uploadAnswer(quiz,answer){
 		$.ajax({
-        	type: 'GET',
+        	type: 'POST',
         	url: 'api/index.php',
         	dataType: 'json',
         	cache: false,
@@ -234,11 +307,11 @@ $(document).ready(function(){
 				'utm_medium': ''
         	},
         	success:function(result){
-        		if(result){
+        		if(result == 1){
         			console.log('upload answer successfully');
         		}else if(result === 100){
         			console.log('activity is timeout');
-        		}else{
+        		}else if(result == 0){
         			console.log('upload answer failed');
         		}
         	}
@@ -252,5 +325,132 @@ $(document).ready(function(){
         }else{ 
             return false; 
         } 
+	}
+
+	function getVertifyCode(phoneNumber){
+		$.ajax({
+			type: 'POST',
+        	url: 'api/index.php',
+        	dataType: 'json',
+        	cache: false,
+        	data:{
+        		'action':'captcha',
+        		'mobile': phoneNumber,
+        		'utm_source': '',
+				'utm_medium': ''
+        	},
+        	success:function(result){
+        		if(result == 1){
+        			alert('验证码已发送,请注意查收短信');
+        		}else if(result == 0){
+        			alert('抱歉，验证码发送失败');
+        		}else if(result == 100){
+        			alert('该活动已过期');
+        		}
+        	}
+		});
+	}
+
+	function submitInfo(username,phoneNumber, code){
+		$.ajax({
+			type: 'POST',
+        	url: 'api/index.php',
+        	dataType: 'json',
+        	cache: false,
+        	data:{
+        		'action':'add_campaign',
+        		'mobile': phoneNumber,
+        		'user_name': username,
+        		'captcha': code,
+        		'utm_source': '',
+				'utm_medium': ''
+        	},
+        	success:function(result){
+        		if(result == 1){
+        			//中奖
+        			JC.getAward = true;
+        			$('.resultWrap .awardWrap  .info').hide();
+        			$('.resultWrap .awardWrap  .award').fadeIn();
+        		}else if(result == 0){
+        			//未中奖
+        			$('.resultWrap .awardWrap .info').hide();
+        			$('.resultWrap .awardWrap  .noAward').fadeIn();
+        		}else if(result == 4){
+        			//验证码错误
+        			alert('验证码错误');
+        			return;
+        		}else if(result == 100){
+        			//不在活动期内
+        			alert('该活动已过期');
+        			return;
+        		}
+        	}
+		});
+	}
+
+	function uploadAddress(address){
+		$.ajax({
+			type: 'POST',
+        	url: 'api/index.php',
+        	dataType: 'json',
+        	cache: false,
+        	data:{
+        		'action':'update_user',
+        		'user_address': address,
+        		'utm_source': '',
+				'utm_medium': ''
+        	},
+        	success:function(result){
+        		if(result == 1){
+        			//更新成功
+        			$('.resultWrap .awardWrap .award').hide();
+        			$('.resultWrap .awardWrap .infoCompleted').fadeIn();
+        		}else if(result == 0){
+        			//更新失败
+        			$('.resultWrap .awardWrap .award').hide();
+        			$('.resultWrap .awardWrap .infoinfoCompleted').fadeIn();
+        		}else if(result == 100){
+        			alert('该活动已过期');
+        			return;
+        		}
+        	}
+		});
+	}
+
+	function getOpenId(){
+		$.ajax({
+			type: 'GET',
+        	url: 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN',
+        	dataType: 'json',
+        	cache: false,
+        	success:function(data){
+        		console.log(data);
+        		if(data.openid){
+        			console.log('openid: ' +data.openid);
+        			JC.openid = data.openid;
+        			$.ajax({
+        				type: 'POST',
+        				url: 'api/index.php',
+        				dataType: 'json',
+        				cache: false,
+        				data:{
+        					action:'add_user',
+        					openid: data.openid,
+        					utm_source: '',
+        					utm_medium: ''
+        				},
+        				success:function(result){
+        					if(result == 1){
+        						console.log('upload openid succssfully');
+        					}else if(result == 0){
+        						console.log('upload openid failed');
+        					}else if(result == 100){
+        						console.log('activity timeouted');
+        					}
+        				}
+        			});
+        		}
+        	}
+		})
 	}
 });
